20c8bc7486f5bc3d4e8d832ab2f30f438eb8f653
Fenris-c <anatolijv236@gmail.com>
2019-10-03 16:20:48 +0300

    new server

--- app/src/main/java/com/petersburg_studio/prazdnikraduga/Constants.java
+++ app/src/main/java/com/petersburg_studio/prazdnikraduga/Constants.java
@@ -2,7 +2,7 @@ package com.petersburg_studio.prazdnikraduga;
 
 public class Constants {
 
-    public static final String BASE_URL = "http://ci85258.tmweb.ru/api/";
+    public static final String BASE_URL = "http://f0340795.xsph.ru/api/";
     public static final String API_KEY = "2574f698-97db-48a9-b592-806eadfc5d00";
     public static final int PAGE_SIZE = 10;
     public static final int FIRST_PAGE = 1;
